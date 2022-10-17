e6f6aa5088a625a0c7a872a1d4fa0893ed00c8f2
Roman Hruška <hruska.r@gmail.com>
2022-10-17 21:38:39 +0200

    added test

---
+++ app/src/androidTest/kotlin/com/example/cupcake/ViewModelTests.kt
@@ -0,0 +1,30 @@
+
+package com.example.cupcake
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.cupcake.model.OrderViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class ViewModelTests {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Test
+    fun test_quantity_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.quantity.observeForever {}
+        viewModel.setQuantity(12)
+        assertEquals(12, viewModel.quantity.value)
+    }
+
+    @Test
+    fun test_price_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.setQuantity(12)
+        viewModel.price.observeForever {}
+        assertEquals("$27.00", viewModel.price.value)
+    }
+}
